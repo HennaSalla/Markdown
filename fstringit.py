4be90a816cb1fc09a946bb692bdde1383f672073
HennaSalla <51696@edu.raseko.fi>
2025-01-08 15:00:20 +0200

    Harjoituksia 8.1.2025

---
+++ fstringit.py
@@ -0,0 +1,15 @@
+# MUUTUJAT MERKKIJONOSSA
+# ======================
+
+# Sanakirjat
+henkilo1 = {'etunimi': 'Tiina', 'ika': 27}
+henkilo2 = {'etunimi': 'Jaana', 'ika': 44}
+henkilo3 = {'etunimi': 'Iida', 'ika': 7}
+
+# Perinteinen ratkaisu
+print(henkilo1['etunimi'] + 'n', 'ikä on', henkilo1['ika'], 'vuotta')
+
+# Samaa muotoilua merkkijonona (fstring)
+
+muotoilu_merkkijono = f'{henkilo1["etunimi"]}n ikä on {henkilo1["ika"]}  vuotta'
+print(muotoilu_merkkijono)
