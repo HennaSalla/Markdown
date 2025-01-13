f0ff2d45ee6a2cef8f4210e761fe157b20d89c89
HennaSalla <51696@edu.raseko.fi>
2025-01-13 12:44:19 +0200

    harjoituksia 13.1.2025

--- periytyminen.py
+++ periytyminen.py
@@ -11,15 +11,21 @@ from oliot import Student # Tuodaan oliot moduulista Student-luokka
 # -----------------------------------------------
 class Person():
     """Common class for all person in Raseko"""
-    def __init__(self, etunimi: str, sukunimi: str):
+    def __init__(self, givenName: str, surName: str):
         """Creates a Person object
 
         Args:
-            etunimi (str): A first name
+            egivenName (str): A first name
             sukunimi (str): A last name
         """
-        self.etunimi = etunimi
-        self.sukunimi = sukunimi
+        self.givenName = givenName
+        self.surName = surName
+
+    def calculateAge3(self, isoBirthday: str) -> float:
+        birthday = datetime.datetime.fromisoformat(isoBirthday)
+        age = datetime.datetime.now() - birthday
+        ageInYears = age.days / 365
+        return ageInYears
 
     # Staattinen metodi, joka laskee iän.Staattisessa metodissa ei luoda oliota laikaan vaan metodia voi käyttää suoraan luokasta käsin 
     @staticmethod
@@ -54,16 +60,16 @@ class Person():
 # ---------------------------------------------
 class RasekoStudent(Person):
     """The students class, inherits The Person class"""
-    def __init__(self, etunimi: str, sukunimi: str, opiskelijanumero: str):
+    def __init__(self, givenName: str, surName: str, studentNumber: str):
         """Creates a student object
 
         Args:
-            etunimi (str): Opiskelijan etunimi
-            sukunimi (str): Opiskelijan sukunimi
-            opiskelijanumero (str): Opiskelijanumero
+            giveName (str): Student's first name
+            surName (str): Student's last name
+            studentNumber (str): Student's ID
         """
-        super().__init__(etunimi, sukunimi) # Määritellään tapahtuvaksi yliluokan mukaan
-        self.opiskelijanumero = opiskelijanumero # Ei määritelty yliluokassa
+        super().__init__(givenName, surName) # Määritellään tapahtuvaksi yliluokan mukaan
+        self.studentNumber = studentNumber # Ei määritelty yliluokassa
 
 class RasekoTeacher(Person):
     """Create a teacher inheriting the Person class
@@ -71,26 +77,26 @@ class RasekoTeacher(Person):
     Args:
         Person (class): Name of the super class
     """
-    def __init__(self, etunimi: str, sukunimi: str, luokat: list[str]):
+    def __init__(self, givenName: str, surName: str, group: list[str]):
         """Creates a teacher objects
 
         Args:
-            etunimi (str): Teacher's given name
-            sukunimi (str): Teacher's surname
-            luokat (list[str]): List of student goups
+            givenName (str): Teacher's given name
+            surName (str): Teacher's surname
+            group (list[str]): List of student goups
         """
-        super().__init__(etunimi, sukunimi)
-        self.luokat = luokat
+        super().__init__(givenName, surName)
+        self.group = group
 
     
 
 if __name__ == "__main__":
     rasekoStudent = RasekoStudent('Jonne', 'Janttari', '456543')
-    print(rasekoStudent.sukunimi)  
+    print(rasekoStudent.surName)  
 
     luokat = ['Tivi23A', 'TiVi23B', 'TiVi20oa']
     rasekoTeacher = RasekoTeacher('Markku', 'Kynsijärvi', luokat)
-    print(f'{rasekoTeacher.etunimi} opettaa ryhmiä {rasekoTeacher.luokat}')
+    print(f'{rasekoTeacher.givenName} opettaa ryhmiä {rasekoTeacher.group}')
 
     # Luodaan moduulista oliot.py Student-olio
     student = Student('Tuittu Kiukkunen', 'Auto22B', '2007-10-23')
@@ -101,3 +107,7 @@ if __name__ == "__main__":
 
     ika2 = Person.caculateAge2('1978-12-10')
     print(ika2)
+
+    person1 = Person('Calle', 'Keckelbergs')
+    ika3 = person1.calculateAge3('2009-10-22')
+    print(f'Henkilön {person1.givenName} ikä on {ika3} vuotta')
